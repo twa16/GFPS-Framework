5dee1e4bf269f4b0b2870edcc6cf3f5c24ca867e
MANUEL <MANUEL@DESKTOP1-WIN8>
2013-02-09 15:01:50 -0500

    Initial Commit

---
+++ src/main/java/org/mgenterprises/java/bukkit/gmcfps/App.java
@@ -0,0 +1,13 @@
+package org.mgenterprises.java.bukkit.gmcfps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
